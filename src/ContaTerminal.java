1bd88e432185d52d8e7e6507ed47c861f48128d8
ronneyTorres <ronneypris26@gmail.com>
2024-05-30 13:36:43 -0300

    Feito a interação com o usuário através do Scanner e impresso na tela suas informações. Como foi pedido pela atividade.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,15 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
   public static void main(String[] args) {
-    int conta;
-    String agencia;
-    String nome;
-    double saldo;
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-    System.out.println();
-    
+    System.out.println("Olá, qual seu nome completo?");
+    String nome = scanner.nextLine();   
 
+    System.out.println("Informe o número da sua agência.");
+    String agencia = scanner.next();
 
+    System.out.println("Informe o número da sua conta.");
+    int conta = scanner.nextInt();
 
-    System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é  " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+    System.out.println("Informe o valor que deseja depósitar?");
+    Double saldo = scanner.nextDouble();
+
+    System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo é " + saldo + ", já está disponível para saque");
+
   }
 }

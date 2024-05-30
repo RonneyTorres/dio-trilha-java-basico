9988e29f3372623e7d2190e6230bc3abc2a29803
ronneyTorres <ronneypris26@gmail.com>
2024-05-30 12:16:43 -0300

    Iniciando a atividade Iniciando uma conta bancária através do terminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,15 @@
+public class ContaTerminal {
+  public static void main(String[] args) {
+    int conta;
+    String agencia;
+    String nome;
+    double saldo;
+
+    System.out.println();
+    
+
+
+
+    System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é  " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+  }
+}
